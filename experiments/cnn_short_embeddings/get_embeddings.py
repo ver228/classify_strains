09b2b6d04edfe19f57c3b18b8cee7dcb03b41862
ver228 <ajaver@MRC-8791.local>
2017-11-29 15:11:12 +0000

    update embeddings collect

--- experiments/cnn_short_embeddings/get_embeddings.py
+++ experiments/cnn_short_embeddings/get_embeddings.py
@@ -10,9 +10,9 @@ import os
 import pandas as pd
 import torch
 import tqdm
-from torch import nn
-import torch.nn.functional as F
-    
+import numpy as np
+import tables
+
 #Be sure to use abspath linux does not give the path if one uses __file__
 _BASEDIR = os.path.dirname(os.path.abspath(__file__))
 src_dir = os.path.join(_BASEDIR, os.pardir, os.pardir, 'src')
@@ -22,132 +22,126 @@ from classify.flow import SkeletonsFlowFull, get_valid_strains, get_datset_file
 import classify.models.model_w_embedding as models
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     dname = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/trained_models'
     
-    model_name = 'resnet18_w_emb_R_L256_l2_0.1_20171126_010058_best.pth.tar'
-    model_name = 'simple_w_emb_R_L256_l2_0.01_20171126_010327_best.pth.tar'
+    #model_file = 'resnet18_w_emb_R_L256_l2_0.1_20171126_010058_best.pth.tar'
+    #props = dict(
+    #        model_name = 'resnet18_w_emb',
+    #        is_residual = True,
+    #        embedding_size = 256
+    #        )
     
+    model_file = 'simple_w_emb_R_L256_l2_0.01_20171126_010327_best.pth.tar'
+    props = dict(
+            model_name = 'simple_w_emb',
+            is_residual = True,
+            embedding_size = 256
+            )
+    
+    
+    dataset = 'CeNDR'
+    valid_strains = get_valid_strains(dataset, is_reduced=True)
+    data_file = get_datset_file(dataset)
+    gen = SkeletonsFlowFull(
+                          n_batch = 32, 
+                          data_file = data_file,
+                          sample_size_seconds = 10, 
+                          sample_frequency_s = 0.04,
+                          valid_strains = valid_strains,
+                          label_type = 'row_id',
+                          is_return_snps = False,
+                          transform_type = 'angles'
+                          )
+    
+    get_model_func = getattr(models, props['model_name'])
+    model = get_model_func(gen, props['embedding_size'])
+    
+    fname = os.path.join(dname, model_file)
+    checkpoint = torch.load(fname, map_location=lambda storage, loc: storage)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()
 
-
-#class HeadlessModel(nn.Module):
-#    def __init__(self, original_model):
-#        super().__init__()
-#        self.features = nn.Sequential(
-#            # stop at conv4
-#            *list(original_model.children())[:-1]
-#        )
-#    def forward(self, x):
-#        x = self.features(x)
-#        x = F.max_pool2d(x, kernel_size=x.size()[2:])
-#        x = x.view(x.size(0), -1)
-#        return x
-
-#if __name__ == '__main__':
-#    import tables
-#    import numpy as np
-#    
-#    models_path = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/logs/model_20171103/'
-#    model_name = 'resnet50_R_CeNDR_ang__S10_F0.04_20171104_182812.pth.tar'
-#    dataset = 'CeNDR'
-#    n_classes = 197
-#    
-#    
-#    fname = os.path.join(models_path, model_name)
-#    model = ResNetS(Bottleneck, [3, 4, 6, 3], n_channels=1, num_classes = n_classes)
-#    checkpoint = torch.load(fname, map_location=lambda storage, loc: storage)
-#    model.load_state_dict(checkpoint['state_dict'])
-#    model.eval()
-#    
-#    
-#    data_file = get_datset_file(dataset)
-#    valid_strains = get_valid_strains(dataset, is_reduced=True)
-#    
-#    dd = model_name.split('_')
-#    sample_size_seconds = [float(x[1:]) for x in dd if x.startswith('S')][0]
-#    sample_frequency_s = [float(x[1:]) for x in dd if x.startswith('F')][0]
-#    
-#    gen = SkeletonsFlowFull(
-#                          n_batch = 32, 
-#                          data_file = data_file,
-#                          sample_size_seconds = sample_size_seconds, 
-#                          sample_frequency_s = sample_frequency_s,
-#                          valid_strains = valid_strains,
-#                          is_torch = True,
-#                          label_type = 'row_id'
-#                          )
-#    results = []
-#    
-#    model_headless = HeadlessModel(original_model=model)
-#    for input_v, row_ids in tqdm.tqdm(gen):
-#        embedding = model_headless.forward(input_v)
-#        results.append((row_ids.data.numpy(), embedding.data.numpy()))
-#    
-#    labels, embeddings = zip(*results)
-#    #%%
-#    
-#    embeddings = np.concatenate(embeddings)
-#    labels = np.concatenate(labels)
-#    #%%
-#    df_g = pd.DataFrame(labels.T, columns=['row_id']).groupby('row_id').groups
-#    #%%
-#    with pd.HDFStore(data_file, 'r') as fid:
-#        skeletons_groups = fid['/skeletons_groups']
-#    
-#    #%%
-#    embedding_groups = []
-#    for irow, row in skeletons_groups.iterrows():
-#        if irow in df_g:
-#            row_n = row.copy()
-#            row_n['ini'] = df_g[irow].min()
-#            row_n['fin'] = df_g[irow].max()
-#            row_n['skel_group_id'] = irow
-#            embedding_groups.append(row_n)
-#    embedding_groups = pd.DataFrame(embedding_groups)
-#    
-#    #%%
-#    TABLE_FILTERS = tables.Filters(
-#        complevel=5,
-#        complib='zlib',
-#        shuffle=True,
-#        fletcher32=True)
-#    
-#    new_file = data_file.replace('_skel_smoothed.hdf5', '_embedings.hdf5')
-#    fields2copy = ['experiments_data', 'snps_data', 'strains_codes']
-#    
-#    with tables.File(data_file, 'r') as fid_old, \
-#            tables.File(new_file, "w") as fid_new:
-#        
-#        for field in fields2copy:
-#            tab = fid_old.get_node('/' +field)[:]        
-#            fid_new.create_table('/', 
-#                             field,
-#                             obj=tab,
-#                             filters=TABLE_FILTERS)
-#        gg = fid_new.create_group('/', 'index_groups')
-#        for field in ['train', 'test', 'val']:
-#            tab = fid_old.get_node('/index_groups/' +field)[:]        
-#            fid_new.create_array('/index_groups', 
-#                             field,
-#                             obj=tab)
-#    #%%
-#    
-#    with tables.File(new_file, "r+") as fid:
-#        table_type = np.dtype([('experiment_id', np.int32),
-#                               ('worm_index', np.int32),
-#                          ('strain', 'S10'),
-#                          ('ini_time_aprox', np.float32),
-#                          ('ini', np.int32),
-#                          ('fin', np.int32),
-#                          ('skel_group_id', np.int32)
-#                          ])
-#        em = embedding_groups.to_records(index=False).astype(table_type)
-#        fid.create_table('/', 
-#                             'embedding_groups',
-#                             obj=em,
-#                             filters=TABLE_FILTERS)
-#        fid.create_carray('/', 
-#                             'embeddings',
-#                             obj=embeddings,
-#                             filters=TABLE_FILTERS)
-#    
+    results = []
+    for ii, (input_v, row_ids) in enumerate(tqdm.tqdm(gen)):
+        video_embedding = model.video_model(input_v)
+        pred = model.classification(video_embedding).max(1)[1]
+        dat = [x.data.numpy() for x in (row_ids, video_embedding, pred)]    
+        results.append(dat)
+        
+    #%%
+    row_ids, embeddings, predictions = map(np.concatenate, zip(*results))
+    
+    df_g = pd.DataFrame(row_ids.T, columns=['row_id']).groupby('row_id').groups
+    embedding_groups = []
+    for irow, row in gen.skeletons_ranges.iterrows():
+        if irow in df_g:
+            row_n = row[['experiment_id',  'worm_index', 'strain', 'strain_id']].copy()
+            row_n['ini'] = df_g[irow].min()
+            row_n['fin'] = df_g[irow].max()
+            row_n['skel_group_id'] = irow
+            embedding_groups.append(row_n)
+    embedding_groups = pd.DataFrame(embedding_groups)
+    #%%
+    snps_embeddings = np.full((gen.n_classes, props['embedding_size']), np.nan, dtype=np.float32)
+    
+    for strain_id in gen.strain_ids:
+        strain = gen.strain_codes.loc[strain_id, 'strain']
+        snps = gen.snps_data[strain].values.T.astype(np.float32)
+        
+        snps = torch.from_numpy(snps).float()
+        snps = torch.autograd.Variable(snps)
+        snps_embedding = model.snp_mapper(snps)
+        snps_embeddings[strain_id] = snps_embedding.data.numpy()
+    #%%
+    
+    TABLE_FILTERS = tables.Filters(
+        complevel=5,
+        complib='zlib',
+        shuffle=True,
+        fletcher32=True)
+    
+    new_file = data_file.replace('_skel_smoothed.hdf5', '_embedings.hdf5')
+    fields2copy = ['experiments_data', 'snps_data', 'strains_codes']
+    
+    with tables.File(data_file, 'r') as fid_old, \
+            tables.File(new_file, "w") as fid_new:
+        
+        for field in fields2copy:
+            tab = fid_old.get_node('/' +field)[:]        
+            fid_new.create_table('/', 
+                             field,
+                             obj=tab,
+                             filters=TABLE_FILTERS)
+        gg = fid_new.create_group('/', 'index_groups')
+        for field in ['train', 'test', 'val']:
+            tab = fid_old.get_node('/index_groups/' +field)[:]        
+            fid_new.create_array('/index_groups', 
+                             field,
+                             obj=tab)
+    #%%
+    with tables.File(new_file, "r+") as fid:
+        table_type = np.dtype([('experiment_id', np.int32),
+                               ('worm_index', np.int32),
+                          ('strain', 'S10'),
+                          ('ini', np.int32),
+                          ('fin', np.int32),
+                          ('skel_group_id', np.int32)
+                          ])
+        em = embedding_groups[list(table_type.names)].to_records(index=False).astype(table_type)
+        fid.create_table('/', 
+                             'embedding_groups',
+                             obj=em,
+                             filters=TABLE_FILTERS)
+        fid.create_carray('/', 
+                             'video_embeddings',
+                             obj=embeddings,
+                             filters=TABLE_FILTERS)
+        fid.create_carray('/', 
+                             'predicted_strain_id',
+                             obj=predictions,
+                             filters=TABLE_FILTERS)
+        fid.create_carray('/', 
+                             'snps_embeddings',
+                             obj=snps_embeddings,
+                             filters=TABLE_FILTERS)
