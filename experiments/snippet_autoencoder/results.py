2d15127600c8472b8d682946dd0d923c98202d88
ver228 <ver228@gmail.com>
2017-11-30 11:21:59 +0000

    test

--- experiments/snippet_autoencoder/results.py
+++ experiments/snippet_autoencoder/results.py
@@ -22,7 +22,9 @@ if __name__ == '__main__':
     mask_file = os.path.join(data_dir,fname)
     feat_file = os.path.join(data_dir,fname.replace('.hdf5', '_featuresN.hdf5'))
     
+    #model_file = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/logs/snippet_autoencoder/AE3D__tiny2L256_20171129_171930/checkpoint.pth.tar'
     model_file = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/logs/snippet_autoencoder/AE3D_L256_20171128_150429/checkpoint.pth.tar'
+    #model_file = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/logs/snippet_autoencoder/AE3D_L256_20171129_155641/checkpoint.pth.tar'
     model = AE3D(256)
     
     checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
@@ -34,55 +36,57 @@ if __name__ == '__main__':
                              roi_size = 128,
                              batch_size = 4,
                              snippet_size = 255,
-                             max_n_frames=2
+                             max_n_frames=-1
                              )
     import time
     tic = time.time()
-    for S in generator:
+    for ii, S in enumerate(generator):
         print('G', time.time()-tic)
         tic = time.time()
+        if ii > 10:
+            break
         
-        output = model(S)
-        print('M', time.time()-tic)
-        tic = time.time()
-        break
-    
-    
-    #%%
-    dpi = 100
-    def ani_frame(n_batch):
-        
-        ori = S.data.squeeze().numpy()[n_batch]
-        dat = output.data.squeeze().numpy()[n_batch]
-        
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_aspect('equal')
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-    
-        dd = np.hstack((ori[0], dat[0]))
-        im = ax.imshow(dd,cmap='gray',interpolation='nearest')
-        im.set_clim([0,1])
-        fig.set_size_inches([8,4])
-        plt.tight_layout()
-        
-    
-        
-    
-    
-        def update_img(n):
-            tmp = np.hstack((ori[n], dat[n]))
-            im.set_data(tmp)
-            return im
-    
-        #legend(loc=0)
-        ani = animation.FuncAnimation(fig,update_img, 128, interval = 40)
-        writer = animation.writers['ffmpeg'](fps = 25)
-    
-        ani.save('demo{}.mp4'.format(n_batch),writer=writer,dpi=dpi)
-        return ani
-    
-    for kk in range(S.size(0)):
-        ani = ani_frame(kk)
-        plt.show()
+#        output = model(S)
+#        print('M', time.time()-tic)
+#        tic = time.time()
+#        break
+#    
+#    
+#    #%%
+#    dpi = 100
+#    def ani_frame(n_batch):
+#        
+#        ori = S.data.squeeze().numpy()[n_batch]
+#        dat = output.data.squeeze().numpy()[n_batch]
+#        
+#        fig = plt.figure()
+#        ax = fig.add_subplot(111)
+#        ax.set_aspect('equal')
+#        ax.get_xaxis().set_visible(False)
+#        ax.get_yaxis().set_visible(False)
+#    
+#        dd = np.hstack((ori[0], dat[0]))
+#        im = ax.imshow(dd,cmap='gray',interpolation='nearest')
+#        im.set_clim([0,1])
+#        fig.set_size_inches([8,4])
+#        plt.tight_layout()
+#        
+#    
+#        
+#    
+#    
+#        def update_img(n):
+#            tmp = np.hstack((ori[n], dat[n]))
+#            im.set_data(tmp)
+#            return im
+#    
+#        #legend(loc=0)
+#        ani = animation.FuncAnimation(fig,update_img, 128, interval = 40)
+#        writer = animation.writers['ffmpeg'](fps = 25)
+#    
+#        ani.save('demo{}.mp4'.format(n_batch),writer=writer,dpi=dpi)
+#        return ani
+#    
+#    for kk in range(S.size(0)):
+#        ani = ani_frame(kk)
+#        plt.show()
