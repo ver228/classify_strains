36dde9e8abe699df70fdc70f3d3bc87ead0d3e37
ver228 <ajaver@MRC-8791.local>
2017-11-30 16:55:49 +0000

    cuda bug

--- experiments/worm_autoencoder/results.py
+++ experiments/worm_autoencoder/results.py
@@ -53,6 +53,36 @@ def results_AE(mask_file, feat_file):
         all_imgs.append((n, imgs))
     
     all_imgs = sorted(all_imgs, key = lambda x : x[0])
+#    #%%
+#    m, mod = all_models[0]
+#    embedding =  mod.encoder(X)
+#    decoded = mod.decoder(embedding)
+#    
+#    n1,n2 = 1, 25
+#    xd = decoded.data.squeeze().numpy()
+#    
+#    plt.figure(figsize=(15, 5))
+#    plt.subplot(1,4,1)
+#    plt.imshow(xd[n1], interpolation=None, cmap='gray')
+#    plt.title('A')
+#    
+#    plt.subplot(1,4,2)
+#    plt.imshow(xd[n2], interpolation=None, cmap='gray')
+#    plt.title('B')
+#    
+#    plt.subplot(1,4,3)
+#    z = (embedding[n1]+embedding[n2]).view(1, -1)
+#    zd = mod.decoder(z).data.squeeze().numpy()
+#    plt.imshow(zd, interpolation=None, cmap='gray')
+#    plt.title('A + B')
+#    
+#    plt.subplot(1,4,4)
+#    z = (embedding[n1]-embedding[n2]).view(1, -1)
+#    zd = mod.decoder(z).data.squeeze().numpy()
+#    plt.imshow(zd, interpolation=None, cmap='gray')
+#    plt.title('A - B')
+    
+    
     
     #%%
     n_models = len(all_imgs)
@@ -66,7 +96,7 @@ def results_AE(mask_file, feat_file):
             plt.title(n)
         plt.savefig('AE_{}.png'.format(ii))
         
-def results_VAE():
+def results_VAE(mask_file, feat_file):
     #model_path = os.path.join(model_dir_root, 'VAE_L64_20171124_144333', 'checkpoint.pth.tar')
     #model = VAE(64)
     #%%
@@ -108,7 +138,34 @@ def results_VAE():
         all_imgs.append((n, imgs))
     
     all_imgs = sorted(all_imgs, key = lambda x : x[0])
+    #%%
+    m, mod = all_models[0]
+    embedding, _, _ =  mod.encoder(X)
+    decoded = mod.decoder(embedding)
     
+    n1,n2 = 18, 25
+    xd = decoded.data.squeeze().numpy()
+    
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1,4,1)
+    plt.imshow(xd[n1], interpolation=None, cmap='gray')
+    plt.title('A')
+    
+    plt.subplot(1,4,2)
+    plt.imshow(xd[n2], interpolation=None, cmap='gray')
+    plt.title('B')
+    
+    plt.subplot(1,4,3)
+    z = (embedding[n1]+embedding[n2]).view(1, -1)
+    zd = mod.decoder(z).data.squeeze().numpy()
+    plt.imshow(zd, interpolation=None, cmap='gray')
+    plt.title('A + B')
+    
+    plt.subplot(1,4,4)
+    z = (embedding[n1]-embedding[n2]).view(1, -1)
+    zd = mod.decoder(z).data.squeeze().numpy()
+    plt.imshow(zd, interpolation=None, cmap='gray')
+    plt.title('A - B')
     #%%
     n_models = len(all_imgs)
     for ii, ori in enumerate(cx):
@@ -119,18 +176,18 @@ def results_VAE():
             plt.subplot(1,n_models+1, n_i+2)
             plt.imshow(imgs[ii], interpolation=None, cmap='gray')
             plt.title(n)        
-        plt.savefig('VAE_{}.png'.format(ii))
+        #plt.savefig('VAE_{}.png'.format(ii))
         
     
         #%%
 if __name__ == '__main__':
-    #from train import feat_file, mask_file
+    from train import mask_file
     #mask_file = '/Volumes/behavgenom_archive$/Avelino/screening/CeNDR/MaskedVideos/CeNDR_Set1_190517/QX1794_worms10_food1-10_Set5_Pos4_Ch4_19052017_125059.hdf5'
-    mask_file = '/Users/ajaver/OneDrive - Imperial College London/optogenetics/Arantza/MaskedVideos/control_pulse/pkd2_2min_Ch1_11052017_120432.hdf5'
+    #mask_file = '/Users/ajaver/OneDrive - Imperial College London/optogenetics/Arantza/MaskedVideos/control_pulse/pkd2_2min_Ch1_11052017_120432.hdf5'
     feat_file = mask_file.replace('MaskedVideos', 'Results').replace('.hdf5', '_featuresN.hdf5')
 
 
-    results_AE(mask_file, feat_file)
+    results_VAE(mask_file, feat_file)
     
     #decoded, mu, _ = model.forward(X)
     #z, _, _ = model.encoder(X)
