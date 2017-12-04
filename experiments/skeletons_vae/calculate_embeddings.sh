239dad07a3226248852b4b5aa60349385f6cbab6
ver228 <ver228@gmail.com>
2017-12-04 10:08:06 +0000

    more embeddings

---
+++ experiments/skeletons_vae/calculate_embeddings.sh
@@ -0,0 +1,20 @@
+
+#!/bin/sh
+#PBS -l walltime=24:00:00
+## This tells the batch manager to limit the walltime for the job to XX hours, YY minutes and ZZ seconds.
+
+#PBS -l select=1:ncpus=2:mem=16gb:ngpus=1
+## This tells the batch manager to use NN node with MM cpus and PP gb of memory per node with QQ gpus available.
+
+module load anaconda3
+module load cuda
+## This job requires CUDA support.
+source activate tierpsy
+
+## copy temporary files
+cp $WORK/classify_strains/trained_models/vae_w_embeddings/*_checkpoint.pth.tar $TMPDIR
+cp $WORK/classify_strains/train_set/CeNDR_skel_smoothed.hdf5 $TMPDIR/
+
+python $HOME/classify_strains/experiments/skeletons_vae/get_embeddings.py
+
+cp $TMPDIR/*_embeddings.hdf5 $WORK/classify_strains/trained_models/vae_w_embeddings/
---
+++ experiments/skeletons_vae/rename_model_files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 11:59:28 2017
+
+@author: ajaver
+"""
+
+import os
+import glob
+import shutil
+
+#dst_dir = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/trained_models/ae_w_embeddings'
+#log_dir = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/logs/skeletons_autoencoder'
+
+log_dir = '/work/ajaver/classify_strains/results/skeletons_vae/'
+dst_dir = '/work/ajaver/classify_strains/trained_models/vae_w_embeddings/'
+
+
+fnames = glob.glob(os.path.join(log_dir, '**', 'checkpoint*'), recursive=True)
+
+
+for fname in fnames:
+    parts = fname.split(os.sep)
+    new_name = os.path.join(dst_dir, parts[-2] + '_' + parts[-1])
+    shutil.copyfile(fname, new_name)
